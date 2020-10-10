e769e7f7b9dcfc01bbe727e24293d8299e493237
unknown <ravibs1994@gmail.com>
2020-10-10 22:05:26 +0530

    UC3_DoubletPercentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,8 +3,14 @@ head=1
 tail=0
 hCount=0
 tCOunt=0
+dHCount=0
+dTCount=0
 i=1
+j=1
+#declare Associative Array
 declare -A arr
+declare -A arr1
+#count Singlet
 while [ $i -le 20 ]
 do
 	result=$((RANDOM%2))
@@ -22,3 +28,24 @@ done
 echo ${arr[@]}
 echo "Percentage of Head" $((`awk 'BEGIN{print '$hCount'/'20'*'100'}'`))
 echo "Percentage of Tail" $((`awk 'BEGIN{print '$tCount'/'20'*'100'}'`))
+
+#count doublet
+while [ $j -le 20 ]
+do
+   result=$((RANDOM%2))
+	result1=$((RANDOM%2))
+   if [[ $result -eq $head && $result1 -eq $head ]]
+      then
+         ((dHCount++))
+         arr1[$j]="HH"
+      elif [[ $result -eq $tail && $result1 -eq $tail ]]
+			then
+         ((dTCount++))
+         arr1[$j]="TT"
+
+   fi
+   ((j++))
+done
+echo ${arr1[@]}
+echo "Percentage of Head" $((`awk 'BEGIN{print '$dHCount'/'20'*'100'}'`))
+echo "Percentage of Tail" $((`awk 'BEGIN{print '$dTCount'/'20'*'100'}'`))
