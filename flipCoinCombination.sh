9c58d2d60191dfd1fa12b2e50a32907c78553ef2
unknown <ravibs1994@gmail.com>
2020-10-10 21:46:09 +0530

    UC2_SingletPercentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,24 @@
 #!/bin/bash -x
 head=1
 tail=0
-result=$((RANDOM%2))
-if [ $result -eq $head ]
-	then 
-		echo "Head"
-	else
-		echo "Tail"
-fi
+hCount=0
+tCOunt=0
+i=1
+declare -A arr
+while [ $i -le 20 ]
+do
+	result=$((RANDOM%2))
+	if [ $result -eq $head ]
+		then
+			((hCount++)) 
+			arr[$i]="H"
+		else
+			((tCount++))
+			arr[$i]="T"
+
+	fi
+	((i++))
+done
+echo ${arr[@]}
+echo "Percentage of Head" $((`awk 'BEGIN{print '$hCount'/'20'*'100'}'`))
+echo "Percentage of Tail" $((`awk 'BEGIN{print '$tCount'/'20'*'100'}'`))
